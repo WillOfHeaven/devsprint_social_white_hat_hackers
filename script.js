7978128735dadf7c996e8a23843a9a80a78e863a
Saurav Kumar <2130171@kiit.ac.in>
2022-11-05 12:34:33 +0530

    items and localstorage

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', (e) => {
+    const boxName = "carts";
+    class Item{
+        constructor(id, name, price, floor, section, aisle) {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.floor = floor;
+            this.section = section;
+            this.aisle = aisle;
+        }
+    }
+    //A array which stores items;
+    const items = [];
+    function addItemsToStorage(item) {
+        items.push(item);
+        let jsonItem = JSON.stringify({items: items});
+        localStorage.setItem(boxName, items);
+    }
+
+    function getItemsFromStorage() {
+        let jsonItem = localStorage.getItem(boxName);
+        items.push(...JSON.parse(jsonItem).items);
+    }
+
+    document.addEventListener('dblclick',(e)=> {
+        getItemsFromStorage();
+        
+    })
+
+    
+})
